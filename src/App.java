d9aa91276ab674e0e797b265f3cb2ff59294eddb
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2026-01-20 12:59:43 +0100

    börjar med main metoden

--- src/App.java
+++ src/App.java
@@ -5,7 +5,63 @@ import Byggpaket.*;
 public class App {
     public static void main(String[] args) throws Exception {
     
+    Scanner tb = new Scanner(System.in); // tar in värden.
         
+    Bostäder[] beställningar = new Bostäder[50];
+    int byggnadAntal = 0;
+    int menyval;
+
+    while(true)
+    {
+        System.out.println("Välkommen till Luddes Fastighets och bostadsbyrå!");
+
+        while(true){
+            try 
+            {
+               System.out.println
+               (
+                "1. Lägg till beställning \n2. Ta bort beställning \n3. Lista beställningar \n4. Lista vinst"
+               ); 
+
+               menyval = tb.nextInt();
+
+               if(menyval > 4 || menyval < 1)
+               {
+                System.out.println("Menyvalet får inte vara mindre än 1 eller större än 4");
+                tb.nextLine();
+                continue;
+               }
+
+               else
+               {
+                tb.nextLine();
+                break;
+               }
+            } 
+
+            catch (Exception e) 
+            {
+                System.out.println("inte bokstäver eller mellanslag");
+                tb.nextLine();
+                continue;
+            }
+        }
+
+        
+        switch(menyval)
+        {
+            case 1:
+
+            case 2:
+
+            case 3:
+
+            case 4:
+            
+        }
+    }
+
+
 
 
 
--- src/Byggpaket/Lägenhetshus.java
+++ src/Byggpaket/Lägenhetshus.java
@@ -2,9 +2,68 @@ package Byggpaket;
 import java.util.ArrayList;
 
 public class Lägenhetshus {
-    private int förråd;
+    private int förråd; //hur många förråd som finns
     private int trappor;
+    private String namn;
+    private Lägenhet lägenhet;
     private ArrayList <Lägenhet> lägenheter;
 
-    public 
+    public Lägenhetshus(int förråd, int trappor, Lägenhet lägenhet, ArrayList<Lägenhet> lägenheter)
+    {
+        if(namn == null|| namn.trim().isEmpty())
+        {
+            throw new IllegalArgumentException("namn får inte vara tom eller mellanslag");
+        }
+        this.namn = namn;
+        this.förråd = gränsCheck(förråd, 10, 20);
+        this.trappor = gränsCheck(trappor,1, 6);
+        this.lägenhet = lägenhet;
+        lägenheter.add(lägenhet);
+
+    }
+
+    public void setFörråd(int förråd) {
+        förråd = gränsCheck(förråd,10,20);
+        this.förråd = förråd;
+    }
+    public void setTrappor(int trappor)
+    {
+        trappor = gränsCheck(trappor, 1,6);
+    }
+
+    public void setNamn(String namn)
+    {
+       if(namn == null|| namn.trim().isEmpty())
+        {
+            throw new IllegalArgumentException("namn får inte vara tom eller mellanslag");
+        }  
+        this.namn = namn;
+    }
+
+
+    public int getFörråd() {
+        return this.förråd;
+    }
+
+    public int getTrappor()
+    {
+        return this.trappor;
+    }
+
+    public String getNamn() {
+        return this.namn;
+    }
+
+
+
+
+
+      public int gränsCheck(int checkvärde, int övre, int undre)
+    {
+       if(checkvärde > övre || checkvärde < undre)
+        {
+            throw new IllegalArgumentException(checkvärde + " får inte vara större än " + övre + " eller mindre än " + undre);
+        }
+        return checkvärde;
+    }
 }
